bc5f965a4fd3283051eeb0f2fb9fa2a36e961f92
wuxu <wuxu1019@hotmail.com>
2018-01-31 11:13:18 -0800

    add sort

---
+++ others/sort/bubble_sort.py
@@ -0,0 +1,30 @@
+# bubble sort
+# time complesity is O(n2)
+import sys
+import multiprocessing
+
+def bubble_sort(nums):
+    change = True
+    while change:
+        change = False
+        for i in range(1, len(nums)):
+            if nums[i-1] > nums[i]:
+                nums[i-1], nums[i] = nums[i], nums[i-1]
+                change = True
+    print nums
+    return None
+
+def main():
+    nums = [5, 4, 3, 2, 1]
+    print nums
+    p = multiprocessing.Process(target=bubble_sort, args=(nums,))
+    p.start()
+    p.join(10)
+    if p.is_alive():
+        print 'Process is still alive, let us kill it'
+    p.terminate()
+    p.join()
+    
+if __name__ == '__main__':
+    main()
+
