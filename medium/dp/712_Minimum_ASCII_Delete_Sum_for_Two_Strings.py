c0ce95fb83e72a20ba84ec72a63df087e4515917
wuxu <wuxu1019@hotmail.com>
2018-02-08 18:00:16 -0800

    notice this is a standard dp, how to make the trans function

---
+++ medium/dp/712_Minimum_ASCII_Delete_Sum_for_Two_Strings.py
@@ -0,0 +1,48 @@
+"""
+Given two strings s1, s2, find the lowest ASCII sum of deleted characters to make two strings equal.
+
+Example 1:
+Input: s1 = "sea", s2 = "eat"
+Output: 231
+Explanation: Deleting "s" from "sea" adds the ASCII value of "s" (115) to the sum.
+Deleting "t" from "eat" adds 116 to the sum.
+At the end, both strings are equal, and 115 + 116 = 231 is the minimum sum possible to achieve this.
+Example 2:
+Input: s1 = "delete", s2 = "leet"
+Output: 403
+Explanation: Deleting "dee" from "delete" to turn the string into "let",
+adds 100[d]+101[e]+101[e] to the sum.  Deleting "e" from "leet" adds 101[e] to the sum.
+At the end, both strings are equal to "let", and the answer is 100+101+101+101 = 403.
+If instead we turned both strings into "lee" or "eet", we would get answers of 433 or 417, which are higher.
+Note:
+
+0 < s1.length, s2.length <= 1000.
+All elements of each string will have an ASCII value in [97, 122].
+
+"""
+
+class Solution(object):
+    def minimumDeleteSum(self, s1, s2):
+     
+        dp = [[0] * (len(s2) + 1) for _ in xrange(len(s1) + 1)]
+
+        for i in xrange(len(s1) - 1, -1, -1):
+            dp[i][len(s2)] = dp[i+1][len(s2)] + ord(s1[i])
+        for j in xrange(len(s2) - 1, -1, -1):
+            dp[len(s1)][j] = dp[len(s1)][j+1] + ord(s2[j])
+
+        for i in xrange(len(s1) - 1, -1, -1):
+            for j in xrange(len(s2) - 1, -1, -1):
+                if s1[i] == s2[j]:
+                    dp[i][j] = dp[i+1][j+1]
+                else:
+                    dp[i][j] = min(dp[i+1][j] + ord(s1[i]),
+                                   dp[i][j+1] + ord(s2[j]))
+
+        return dp[0][0]
+        """
+        :type s1: str
+        :type s2: str
+        :rtype: int
+        """
+        
