9ce2468ca5d8ae5ea907fa1fda86cc80d03497f6
wuxu <wuxu1019@hotmail.com>
2018-02-10 22:26:34 -0800

    add code

--- others/mianjing/bst_sum.py
+++ others/mianjing/bst_sum.py
@@ -2,6 +2,30 @@
 give a BST, and min & max, 
 get the sum of BST val, which in the range of min <= val <= max
 """
+class bstNode(object):
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
 
 def sumBST(root, min, max):
+    if not root:
+        return 0
+    if root.val >= min and root.val <= max:
+        rt = root.val
+    else:
+        rt = 0
+    rt += sumBST(root.left, min, max) + sumBST(root.right, min, max)
+    print rt
+    return rt
     
+root = bstNode(1)
+bstNode.left = bstNode(2)
+bstNode.right = bstNode(3)
+bstNode.left.left = 4
+bstNode.left.right = 5
+bstNode.right.left = 6
+bstNode.right.right = 7
+sm = sumBST(root, 2, 5)
+print sm
+
