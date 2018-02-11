8caca5b50a9be990aa3a3a13f73a7cc72035a740
wuxu <wuxu1019@hotmail.com>
2018-02-10 22:59:02 -0800

    fix bug

--- others/mianjing/bst_sum.py
+++ others/mianjing/bst_sum.py
@@ -16,16 +16,15 @@ def sumBST(root, min, max):
     else:
         rt = 0
     rt += sumBST(root.left, min, max) + sumBST(root.right, min, max)
-    print rt
     return rt
     
 root = bstNode(1)
-bstNode.left = bstNode(2)
-bstNode.right = bstNode(3)
-bstNode.left.left = 4
-bstNode.left.right = 5
-bstNode.right.left = 6
-bstNode.right.right = 7
+root.left = bstNode(2)
+root.right = bstNode(3)
+root.left.left = bstNode(4)
+root.left.right = bstNode(5)
+root.right.left = bstNode(6)
+root.right.right = bstNode(7)
 sm = sumBST(root, 2, 5)
 print sm
 
