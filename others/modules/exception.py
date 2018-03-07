9890e70f965a4e4314ce554c164f167012959ec2
wuxu <wuxu1019@hotmail.com>
2018-03-06 17:41:13 -0800

    add finally

--- others/modules/exception.py
+++ others/modules/exception.py
@@ -1,8 +1,21 @@
+
+def IsEqual(a, b):
+
+    if len(a) != len(b):
+        raise Exception('{} is not the same as {}'.format(a, b))
+    for i in range(len(a)):
+        if a[i] != b[i]:
+            raise Exception('{} is not the same as {}'.format(a, b))
+    print '{} is the same as {}'.format(a, b)
+
 def test():
     try:
         i = 3/0 
     except Exception, msg:
-        print "here is some error, see below for details"
+        print "here is some error, see below for details\n\n"
         print msg
+    finally:
+        print "Must go to here"
+        IsEqual('bcc', 'bcc')
 
 test()
