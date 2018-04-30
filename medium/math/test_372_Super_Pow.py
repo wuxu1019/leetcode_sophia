12cc0b4cb05391ddaacc33da1f7a4ed28531da95
wuxu <wuxu1019@hotmail.com>
2018-04-30 10:48:59 -0700

    make use of two principle:
    (a1 * a2) % 1337 = (a1 % 1337) * (a2 % 1337)
    
    pow(a, b) % 1337 = (pow(a, b0 + 10 * b1 + 100 * b2 + 1000 * b3)) % 1337
                     = pow(a, b0) % 1337 * pow(pow(a, 10) % 1337, b1) * pow(pow(a, 100) % 1337, b2)
    Signed-off-by: wuxu <wuxu1019@hotmail.com>

---
+++ medium/math/test_372_Super_Pow.py
@@ -0,0 +1,35 @@
+"""
+Your task is to calculate ab mod 1337 where a is a positive integer and b is an extremely large positive integer given in the form of an array.
+
+Example1:
+
+a = 2
+b = [3]
+
+Result: 8
+Example2:
+
+a = 2
+b = [1,0]
+
+Result: 1024
+Credits:
+Special thanks to @Stomach_ache for adding this problem and creating all test cases.
+"""
+
+class Solution(object):
+    def superPow(self, a, b):
+        """
+        :type a: int
+        :type b: List[int]
+        :rtype: int
+        """
+        def superPowH(a, b, base):
+            if not b:
+                return 1
+            index = b.pop()
+            base *= 10
+            return (pow(a, index, 1337) * superPowH(pow(a, 10, 1337), b, base)) % 1337
+        return superPowH(a, b, 1)
+
+d
