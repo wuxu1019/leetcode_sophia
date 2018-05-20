e5eab4cdeb407b945a8d773259852e2fdb1cf153
wuxu <wuxu1019@hotmail.com>
2018-05-20 07:25:39 -0700

    use dummy for split and merge Signed-off-by: wuxu <wuxu1019@hotmail.com>

---
+++ medium/linklist/test_148_Sort_List.py
@@ -0,0 +1,56 @@
+"""
+Sort a linked list in O(n log n) time using constant space complexity.
+
+Example 1:
+
+Input: 4->2->1->3
+Output: 1->2->3->4
+Example 2:
+
+Input: -1->5->3->4->0
+Output: -1->0->3->4->5
+
+"""
+
+
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution(object):
+    def sortList(self, head):
+        """
+        :type head: ListNode
+        :rtype: ListNode
+        """
+        if not head or not head.next:
+            return head
+        dummy = ListNode(0)
+        dummy.next = head
+        slow, fast = dummy, dummy
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+        slow.next, slow = None, slow.next
+        p1 = self.sortList(dummy.next)
+        p2 = self.sortList(slow)
+        return self.merge(p1, p2)
+
+    def merge(self, head1, head2):
+        dummy = ListNode(0)
+        p = dummy
+        while head1 and head2:
+            if head1.val < head2.val:
+                p.next = head1
+                head1 = head1.next
+            else:
+                p.next = head2
+                head2 = head2.next
+            p = p.next
+        if head1:
+            p.next = head1
+        if head2:
+            p.next = head2
+        return dummy.next
