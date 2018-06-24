63029ab1275d7049e5a59d5fd6e174a0378482a3
wuxu <wuxu1019@hotmail.com>
2018-06-24 15:36:46 -0700

    1, if left height == right height, this means left tree is a full tree 2, if left height > right height, this means right tree is a full tree Signed-off-by: wuxu <wuxu1019@hotmail.com>

---
+++ medium/sortandsearch/test_81_Search_in_Rotated_Sorted_Array_II.py
@@ -0,0 +1,50 @@
+"""
+Suppose an array sorted in ascending order is rotated at some pivot unknown to you beforehand.
+
+(i.e., [0,0,1,2,2,5,6] might become [2,5,6,0,0,1,2]).
+
+You are given a target value to search. If found in the array return true, otherwise return false.
+
+Example 1:
+
+Input: nums = [2,5,6,0,0,1,2], target = 0
+Output: true
+Example 2:
+
+Input: nums = [2,5,6,0,0,1,2], target = 3
+Output: false
+Follow up:
+
+This is a follow up problem to Search in Rotated Sorted Array, where nums may contain duplicates.
+Would this affect the run-time complexity? How and why?
+
+
+"""
+
+class Solution(object):
+    def search(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: bool
+        """
+        lo, hi = 0, len(nums) - 1
+
+        while lo <= hi:
+            mid = lo + (hi - lo) / 2
+            if nums[mid] == target:
+                return True
+            if nums[mid] < nums[hi]:
+                if nums[mid] < target <= nums[hi]:
+                    lo = mid + 1
+                else:
+                    hi = mid - 1
+            elif nums[mid] > nums[hi]:
+                if nums[lo] <= target < nums[mid]:
+                    hi = mid - 1
+                else:
+                    lo = mid + 1
+            else:
+                hi -= 1
+        return False
+
