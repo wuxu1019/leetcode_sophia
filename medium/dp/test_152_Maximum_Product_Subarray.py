f95ea92d08132b87390311a3b8c01111464aa379
wuxu <wuxu1019@hotmail.com>
2018-06-26 15:26:15 -0700

    dp, status is maxinum and mininum product maxinum[i] = max(maxinum[i-1]*num, num, mininum[i-1]*num) mininum[i] = min(mininum[i-1]*num, num, mininum[i-1]*num) Signed-off-by: wuxu <wuxu1019@hotmail.com>

---
+++ medium/dp/test_152_Maximum_Product_Subarray.py
@@ -0,0 +1,29 @@
+"""
+Given an integer array nums, find the contiguous subarray within an array (containing at least one number) which has the largest product.
+
+Example 1:
+
+Input: [2,3,-2,4]
+Output: 6
+Explanation: [2,3] has the largest product 6.
+Example 2:
+
+Input: [-2,0,-1]
+Output: 0
+Explanation: The result cannot be 2, because [-2,-1] is not a subarray.
+
+"""
+
+
+class Solution(object):
+    def maxProduct(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        rt = maxi = mini = nums[0]
+        for num in nums[1:]:
+            maxi, mini = max(num, maxi * num, mini * num), min(num, maxi * num, mini * num)
+
+            rt = max(rt, maxi)
+        return rt
