61cc171a95e49a86886e798c4432a4386c12e3fd
wuxu <wuxu1019@hotmail.com>
2018-07-02 16:22:14 -0700

    use a global variable to remember the last value, inorder travesal Signed-off-by: wuxu <wuxu1019@hotmail.com>

---
+++ medium/tree/test_98_Validate_Binary_Search_Tree.py
@@ -0,0 +1,59 @@
+"""
+Given a binary tree, determine if it is a valid binary search tree (BST).
+
+Assume a BST is defined as follows:
+
+The left subtree of a node contains only nodes with keys less than the node's key.
+The right subtree of a node contains only nodes with keys greater than the node's key.
+Both the left and right subtrees must also be binary search trees.
+Example 1:
+
+Input:
+    2
+   / \
+  1   3
+Output: true
+Example 2:
+
+    5
+   / \
+  1   4
+     / \
+    3   6
+Output: false
+Explanation: The input is: [5,1,4,null,null,3,6]. The root node's value
+             is 5 but its right child's value is 4.
+"""
+
+
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+    def isValidBST(self, root):
+        """
+        :type root: TreeNode
+        :rtype: bool
+        """
+        self.last = -float('INF')
+
+        def inorder(root):
+            if not root:
+                return True
+
+            if root.left:
+                if not inorder(root.left):
+                    return False
+            if root.val <= self.last:
+                return False
+            self.last = root.val
+            if root.right:
+                if not inorder(root.right):
+                    return False
+            return True
+
+        return inorder(root)
