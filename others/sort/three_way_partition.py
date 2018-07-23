f8ee2388002648e32cfb0b3919adb4156609b28f
wuxu <wuxu1019@hotmail.com>
2018-07-23 14:31:52 -0700

    three way divide Signed-off-by: wuxu <wuxu1019@hotmail.com>

--- others/sort/three_way_partition.py
+++ others/sort/three_way_partition.py
@@ -15,7 +15,7 @@ def three_way_partition(l, mid):
     return l
 
 if __name__ == '__main__':
-    l = [1, 4, 1, 3, 4, 6, 2, 3, 4, 5, 6]
-    mid = 4
+    l = [3,2,3,3,2,1,1,2,3,1,1,3,2,1,2,2,2,2,1]
+    mid = 2
     rt = three_way_partition(l, mid)
     print rt
